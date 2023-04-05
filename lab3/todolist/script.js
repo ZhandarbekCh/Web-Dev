4945ed623a4468ddb1117c3601fd6b59514807dc
Zhandarbek Chaliev <zhako.chaliev@mail.ru>
2023-04-05 19:13:24 +0600

    lab8

--- lab3/todolist/script.js
+++ lab3/todolist/script.js
@@ -1,13 +1,14 @@
-
+//Получение всех необходимых элементов
 const inputField = document.querySelector(".input-field textarea"),
   todoLists = document.querySelector(".todoLists"),
   pendingNum = document.querySelector(".pending-num"),
   clearButton = document.querySelector(".clear-button");
 
+//мы будем вызывать эту функцию при добавлении, удалении и проверке-снятии галочки с задачи
 function allTasks() {
   let tasks = document.querySelectorAll(".pending");
 
-  
+  //если длина задач равна 0, то ожидающее текстовое содержимое num будет равно no, если нет, то ожидающее значение num будет длиной задачи
   pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
 
   let allLists = document.querySelectorAll(".list");
@@ -20,9 +21,13 @@ function allTasks() {
   clearButton.style.pointerEvents = "none";
 }
 
-inputField.addEventListener("keyup", (e) => {
-  let inputVal = inputField.value.trim(); 
 
+
+//добавка задачи, пока мы вводим значение в текстовое поле и нажимаем enter
+inputField.addEventListener("keyup", (e) => {
+  let inputVal = inputField.value.trim(); //функция trim удаляет пространство спереди и сзади от введенного значения а то до этого пустота ка цифра кететин еди
+
+  //если нажата кнопка enter и длина входного значения больше 0.
   if (e.key === "Enter" && inputVal.length > 0) {
     let liTag = ` <li class="list pending" onclick="handleStatus(this)">
           <input type="checkbox" />
@@ -30,26 +35,27 @@ inputField.addEventListener("keyup", (e) => {
           <i class="uil uil-trash" onclick="deleteTask(this)"></i>
         </li>`;
 
-    todoLists.insertAdjacentHTML("beforeend", liTag); 
-    inputField.value = ""; 
+    todoLists.insertAdjacentHTML("beforeend", liTag); // добавление ушин керек
+    inputField.value = ""; //удаление значения из поля ввода
     allTasks();
   }
 });
 
-
+//устанавливаем и снимаем флажок, пока мы нажимаем на задачу
 function handleStatus(e) {
-  const checkbox = e.querySelector("input"); 
+  const checkbox = e.querySelector("input"); //getting checkbox
   checkbox.checked = checkbox.checked ? false : true;
   e.classList.toggle("pending");
   allTasks();
 }
 
+//удаляем задачу, пока мы нажимаем на значок удаления.
 function deleteTask(e) {
-  e.parentElement.remove(); 
+  e.parentElement.remove(); //получение родительского элемента и удаление его
   allTasks();
 }
 
-
+//удаляем все задачи, пока мы нажимаем на кнопку Очистить.
 clearButton.addEventListener("click", () => {
   todoLists.innerHTML = "";
   allTasks();
